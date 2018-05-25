5465aa66b8ad471dea8d2c331fe17b22b34b5e28
Max Reeves <reeves.max@gmail.com>
2018-05-25 04:22:46 +0200

    solution to PE 106

--- problem_106/problem_106.py
+++ problem_106/problem_106.py
@@ -1,49 +1,18 @@
-from gecode import *
+import itertools as it
 from time import time
-from itertools import combinations
 
-def max_sol(n):
-    m = [1]
-    for k in range(1, n):
-        m = [m[k/2]] + [m[k/2] + a for a in m]
-    return m
+def le(l):
+  for a,b in l:
+    if a >= b:
+      return False
+  return True
 
 start = time()
+n = 12
+start_set = range(1, n+1)
+subsets = [set(comb) for m in start_set for comb in it.combinations(start_set, m)]
+subset_pairs = [(sorted(list(a)),sorted(list(b))) for a,b in it.combinations(subsets, 2) if not a & b]
+results = [(a,b) for a,b in subset_pairs if len(a) > 1 and len(a) == len(b) and not le(zip(a,b))]
 
-card = 4
-maxSol = max_sol(card)
-maxSum = 100000#sum(maxSol)
-minSum = 1
-maxNum = maxSum
-minNum = 1
-subsetInds = [set(ss) for i in range(1, card+1) for ss in combinations(range(card), i)]
-
-s = space()
-
-A = s.intvars(card, minNum, maxNum)
-subsetCosts = s.intvars(len(subsetInds), minNum, maxSum)
-
-for i in range(card-1):
-    s.rel(A[i], IRT_EQ, A[i+1])
-    
-for i, ssi in enumerate(subsetInds):
-    s.linear([(e in ssi) for e in range(card)], A, IRT_EQ, subsetCosts[i])
-    for j, ssj in enumerate(subsetInds[i+1:], start=i+1):
-        if ssi.isdisjoint(ssj):
-            if len(ssi) == len(ssj):
-                s.rel(subsetCosts[i], IRT_NQ, subsetCosts[j])
-            else:
-                s.rel(subsetCosts[i], IRT_LE, subsetCosts[j])
-
-                
-s.branch(subsetCosts[-1], INT_VAL_MIN)
-s.branch(A, INT_VAR_NONE, INT_VAL_MIN)
-
-print 'Init (sec):', time() - start
-cnt = 0
-for sol in s.search(threads=5, time_limit=30000):
-    cnt += 1
-    print sol.val(subsetCosts[-1]), '== sum(', sol.val(A), ')', cnt
-    
-print 'Time (sec):', time() - start
+print len(results), time()-start
 
